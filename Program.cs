1202c68a2bcdffcc633c71ab5d84d20c74af2b3f
adepra22 <ade.prasetyo@students.amikom.ac.id>
2020-06-10 23:22:56 +0700

    First Commit Abstraction

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Abstraction.AbstractClass;      //Digunakan apabila menggunakan Abstract Class
+using Abstraction.Interface;            //Digunakan apabila menggunakan Interface
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BANGUN RUANG");
+            Console.WriteLine();
+
+            //BangunDatar bangundatar;  //Digunakan apabila menggunakan Abstract Class
+            IBangunDatar bangundatar;   //Digunakan apabila menggunakan Interface
+
+            Console.WriteLine("1. Persegi");
+            bangundatar = new Persegi();
+            bangundatar.Luas();
+            bangundatar.Keliling();
+
+            Console.WriteLine();
+            Console.WriteLine("2. Persegi Panjang");
+            bangundatar = new PersegiPanjang();
+            bangundatar.Luas();
+            bangundatar.Keliling();
+
+            Console.WriteLine();
+            Console.WriteLine("3. Segitiga");
+            bangundatar = new Segitiga();
+            bangundatar.Luas();
+            bangundatar.Keliling();
+
+            Console.ReadKey(true);
+        }
+    }
+}
